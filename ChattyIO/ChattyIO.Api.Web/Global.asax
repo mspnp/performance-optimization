a9fe180fc7384ddafca7600c1dce7ce2aad7fd96
manikrish <manikris@microsoft.com>
2015-02-16 15:18:56 -0800

    chattyio sample with unit tests

---
+++ ChattyIO/ChattyIO.Api.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChattyIO.Api.Web.WebApiApplication" Language="C#" %>
---
+++ ChattyIO/ChattyIO.UI.Web/App_Start/Config.cs
@@ -0,0 +1,13 @@
+﻿namespace ChattyIO.UI.Web.App_Start
+{
+    using System.Configuration;
+
+    public static class Config
+    {
+        static Config()
+        {
+            ApiEndPointBaseAddress = ConfigurationManager.AppSettings["apibaseaddress"];
+        }
+        public static string ApiEndPointBaseAddress { get; set; }
+    }
+}
