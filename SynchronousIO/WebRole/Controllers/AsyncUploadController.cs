417e827b2de49c0b169cb42686eba4639dc12cb5
Matt Johnson <matt.johnson@microsoft.com>
2015-02-23 14:43:50 -0800

    Import System.IO for readability

--- SynchronousIO/WebRole/Controllers/AsyncUploadController.cs
+++ SynchronousIO/WebRole/Controllers/AsyncUploadController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using Microsoft.WindowsAzure;
@@ -24,7 +25,7 @@ namespace WebRole.Controllers
             CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob");
 
             // Create or overwrite the "myblob" blob with contents from a local file.
-            using (var fileStream = System.IO.File.OpenRead(HttpContext.Current.Server.MapPath(@"../FileToUpload.txt")))
+            using (var fileStream = File.OpenRead(HttpContext.Current.Server.MapPath(@"../FileToUpload.txt")))
             {
                 await blockBlob.UploadFromStreamAsync(fileStream).ConfigureAwait(false);
             }
