e473a7a24b453a982af6c4499542d210ea0657e9
Hernan Veiras <hernan.veiras@gmail.com>
2015-03-06 21:20:56 -0300

    -Sample application that compares 3 different ways to access a dictionary concurrently.

---
+++ LockingApplicationData/LockingApplicationData.WebApi/Controllers/ContentionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace LockingApplicationData.WebApi.Controllers
+{
+    public class ContentionController : ApiController
+    {
+        private static IDictionary<string, string> dictionary = new Dictionary<string, string> 
+        {
+            {"key1", "Some Value"}
+        };
+
+        private static ConcurrentDictionary<string, string> concurrentDictionary = new ConcurrentDictionary<string, string>
+        (
+            new Dictionary<string, string>
+            {
+                {"key1", "Some Value"}
+            }
+        );
+
+        private static object _lock = new object();
+
+        [HttpGet]
+        [Route("contention/dictionary_no_locking")]
+        public string GetValueFromDictionaryNoLocking()
+        {
+            return dictionary["key1"];
+        }
+
+        [HttpGet]
+        [Route("contention/dictionary_locking")]
+        public string GetValueFromDictionaryLocking()
+        {
+            lock(_lock)
+            {
+                return dictionary["key1"];
+            }
+        }
+
+        [HttpGet]
+        [Route("contention/concurrent_dictionary")]
+        public string GetValueFromConcurrentDictionaty()
+        {
+            return dictionary["key1"];
+        }
+    }
+}
