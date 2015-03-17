6aea38453c880a0c42af66980eae88d4fc27d9cb
cfarre@microsoft.com <cfarre@microsoft.com>
2015-03-17 13:33:38 -0700

    refactor namespaces & folder

---
+++ BusyDatabase/BusyDatabase.WebApi/Controllers/LessProcSqlController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Threading.Tasks;
+using BusyDatabase.Support;
+
+namespace BusyDatabase.WebApi.Controllers
+{
+    public class LessProcSqlController : ApiController
+    {
+        public async Task GetNameConcat()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.AppSettings["connectionString"]))
+            {
+                await connection.OpenAsync();
+                string commandString = BusyDatabaseUtil.GetQuery("LessSql");                
+                using (SqlCommand command = new SqlCommand(commandString, connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        
+                        while (await reader.ReadAsync())
+                        {
+                            var field = await reader.GetFieldValueAsync<string>(0); 
+                            field=field.Replace(',',' ').Replace("ca","cat"); 
+                            
+                        }
+                    }
+
+
+                }
+            }
+
+
+           
+        }
+
+    }
+}
