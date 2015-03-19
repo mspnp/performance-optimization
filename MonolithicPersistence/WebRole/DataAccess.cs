6eda5d981b6c7fc84f7b5d666b0736e7e00eb7b8
hanzzhang <hanz@microsoft.com>
2015-03-19 13:31:45 -0700

    Add file headers

--- MonolithicPersistence/WebRole/DataAccess.cs
+++ MonolithicPersistence/WebRole/DataAccess.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.ServiceBus.Messaging;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.ServiceBus.Messaging;
 using Microsoft.WindowsAzure;
 using Newtonsoft.Json;
 using System;
