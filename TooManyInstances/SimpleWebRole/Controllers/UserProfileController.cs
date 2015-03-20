072ebc2f010f30dbd6f4b1ebe7fa295c1bbd3ba4
Francis Cheung <fcheung@microsoft.com>
2015-03-20 14:42:57 -0700

    Changed SingleWebRole to not block thread.

--- TooManyInstances/SimpleWebRole/Controllers/UserProfileController.cs
+++ TooManyInstances/SimpleWebRole/Controllers/UserProfileController.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace SimpleWebRole.Controllers
 {
     public class UserProfileController : ApiController
     {
-        public UserProfile Get()
+        public async Task<UserProfile> Get()
         {
             //Simulate processing
-            Thread.Sleep(100);
+            await Task.Delay(100);
 
             return new UserProfile() { FirstName = "Alton", LastName = "Hudgens" };
         }
