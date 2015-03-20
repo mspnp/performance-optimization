c5daa774ac5e12017e398e187c4397e20612799f
Francis Cheung <fcheung@microsoft.com>
2015-03-20 16:10:29 -0700

    Increased the delay in construction for the ExpensiveToCreateService

--- TooManyInstances/WebRole/ExpensiveToCreateService.cs
+++ TooManyInstances/WebRole/ExpensiveToCreateService.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using WebRole.Models;
@@ -16,7 +17,7 @@ namespace WebRole
         public ExpensiveToCreateService()
         {
             //Simulate delay due to setup and configuration of ExpensiveToCreateService
-            Thread.SpinWait(10000);
+            Thread.SpinWait(Int32.MaxValue / 100);
         }
 
         public Task<Product> GetProductByIdAsync(string productId)
