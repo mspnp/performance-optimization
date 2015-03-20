f4265684aeb9bf4f0919a759b93ba0d09ff9fb40
Francis Cheung <fcheung@microsoft.com>
2015-03-20 13:19:37 -0700

    Refactored TooManyInstances into 2: Expensive instantiation and HttpClient style issues.

---
+++ TooManyInstances/WebRole/Controllers/SingleHttpClientInstanceController.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WebRole.Models;
+
+namespace WebRole.Controllers
+{
+    public class SingleHttpClientInstanceController : ApiController
+    {
+        private static readonly HttpClient HttpClient;
+
+        static SingleHttpClientInstanceController()
+        {
+            HttpClient = new HttpClient();
+        }
+
+        /// <summary>
+        /// This method uses the shared instance of HttpClient for every call to GetProductAsync.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Product> GetProductAsync(string id)
+        {
+            var hostName = HttpContext.Current.Request.Url.Host;
+            var result = await HttpClient.GetStringAsync(string.Format("http://{0}:8080/api/userprofile", hostName));
+
+            return new Product { Name = result };
+        }
+    }
+}
