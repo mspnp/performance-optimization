8d0e089714a978c8e622d48ee36103eec279f20e
Matt Johnson <matt.johnson@microsoft.com>
2015-03-23 11:46:53 -0700

    Cleanup unused namespace imports

--- NoCache/WebRole/Controllers/NoCacheController.cs
+++ NoCache/WebRole/Controllers/NoCacheController.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
 using CachingDemo.Data;
-using CachingDemo.Data.Models;
 
 namespace WebRole.Controllers
 {
