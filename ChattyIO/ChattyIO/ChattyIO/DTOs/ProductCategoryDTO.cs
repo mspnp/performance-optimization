a7443708773b0b5ccbdd75ce37750bf040ec1c0a
Federico Arambarri <v-fearam>
2024-10-15 10:11:24 -0300

    upgrade chatty-io

---
+++ ChattyIO/ChattyIO/ChattyIO/DTOs/ProductCategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ChattyIO.DTOs
+{
+    public class ProductCategoryDTO
+    {
+        public int ProductCategoryID { get; set; }
+        public int? ParentProductCategoryID { get; set; }
+        public string Name { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public ICollection<ProductDTO> Products { get; set; }
+    }
+}
---
+++ ChattyIO/ChattyIO/ChattyIO/DTOs/ProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ChattyIO.DTOs
+{
+    public class ProductDTO
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public ICollection<SalesOrderDetailDTO> SalesOrderDetails { get; set; }
+    }
+}
